65ce453e322f065668a5e4d964aa1a5ac846ede6
okaken2022 <okaken.address@gmail.com>
2022-09-21 09:41:34 +0000

    add button

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -128,9 +128,23 @@ var onClickAdd = function onClickAdd() {
   div.className = "list-row"; //liタグ生成
 
   var li = document.createElement("li");
-  li.innerText = inputText; //divタグの子要素に各要素を設定
+  li.innerText = inputText; //button(完了)タグ生成
 
-  div.appendChild(li); //未完了リストに追加
+  var completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", function () {
+    alert("完了");
+  }); //button(削除)タグ生成
+
+  var deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", function () {
+    alert("削除");
+  }); //divタグの子要素に各要素を設定
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton); //未完了リストに追加
 
   document.getElementById("incomplete-list").appendChild(div);
 };
