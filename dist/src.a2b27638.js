18df7261ac3c343c212fdd66656ae5435f0ffce2
okaken2022 <okaken.address@gmail.com>
2022-09-21 09:23:39 +0000

    add input text

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -118,7 +118,26 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/index.js":[function(require,module,exports) {
+var onClickAdd = function onClickAdd() {
+  // inputの内容を取得
+  var inputText = document.getElementById("add-text").value; // 追加ボタンを押すとinputの中が初期化
 
+  document.getElementById("add-text").value = ""; //div生成
+
+  var div = document.createElement("div");
+  div.className = "list-row"; //liタグ生成
+
+  var li = document.createElement("li");
+  li.innerText = inputText; //divタグの子要素に各要素を設定
+
+  div.appendChild(li); //未完了リストに追加
+
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document.getElementById("add-button").addEventListener("click", function () {
+  return onClickAdd();
+});
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
